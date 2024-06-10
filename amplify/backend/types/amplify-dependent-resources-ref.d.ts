ed75c6168750980104cd60cb51d2d8f624cbfb72
Cory Campbell <campbellcory2360@gmail.com>
2024-06-09 20:10:55 -0400

    Added Amplify, need to configure Schema

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "spotifyclone": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
